564cef9bb8b492b8c68ed5a4772c4e8744a540e4
Yago1997-cct <yagoguisilva1997@gmail.com>
2025-05-05 13:45:00 +0100

    firts simple print - homework

--- src/git_2025211/Git_2025211.java
+++ src/git_2025211/Git_2025211.java
@@ -15,6 +15,7 @@ public class Git_2025211 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println(" Yago Guilherme - 2025211");
     }
     
 }
